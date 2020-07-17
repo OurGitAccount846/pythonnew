1c58683f8c66d29818b66e0f270919b9622d9723
OurGitAccount846 <67577459+OurGitAccount846@users.noreply.github.com>
2020-07-17 12:54:17 +0530

    Delete supermarket.py

--- supermarket.py
+++
@@ -1,8 +0,0 @@
-name=input("enter supermarket name")
-item=input("enter item name")
-weight=input("enter weight of item")
-price=input("enter price of the item")
-date=input("enter expiry date of item")
-print("********************************\n \t Supermarket Bill \n *******************************\n name\t\t:{}\n item\t\t:{}\n weight\t\t:{}\n price\t\t:{}\n date\t\t:{}\n ******************************\n".format(name,item,
-weight,price,date))
-
