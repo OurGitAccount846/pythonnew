5cc59ee7a07d1610690937f683e182b582874d0d
OurGitAccount846 <67577459+OurGitAccount846@users.noreply.github.com>
2020-07-17 12:51:42 +0530

    Delete if.py

--- if.py
+++
@@ -1,9 +0,0 @@
-try:
-    getNumber=int(input("enter number:"))
-    reminder= getNumber % 2
-    if(reminder==0):
-     print('even number')
-    else:
-     print('odd')5
-except:
-    print('error')
